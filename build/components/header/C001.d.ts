53accffc85f64ac82e023ac5879571e8489a57a3
Vivek Mengu <vivek.mengu@Viveks-MacBook-Pro.local>
2023-10-13 17:57:55 +0530

    added proprequire

--- build/components/header/C001.d.ts
+++ build/components/header/C001.d.ts
@@ -1,2 +1,26 @@
-declare const C001: () => import("react/jsx-runtime").JSX.Element;
+declare const C001: {
+    (): import("react/jsx-runtime").JSX.Element;
+    propRequire: {
+        title: string;
+        features: (number | {
+            feat_title: string;
+            feat_desc: string;
+            feat_icon: string;
+        })[];
+        user_info: {
+            age: string;
+            addr: {
+                type: string;
+                pincode: (string | number)[];
+            };
+            files: (number | {
+                file_no: string;
+                file_type: {
+                    is_deleted: string;
+                    is_video: string;
+                };
+            })[];
+        };
+    };
+};
 export default C001;

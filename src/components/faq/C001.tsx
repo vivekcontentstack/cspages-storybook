c05ab81be8a375f00e76994fd475c09edd544d88
manoj.muduli <manoj.muduli@contentstack.com>
2023-10-15 22:09:42 +0530

    faq init

---
+++ src/components/faq/C001.tsx
@@ -0,0 +1,11 @@
+import { RequireProps, data } from './defaultValue';
+
+const C001 = () =>{
+    return(
+        <></>
+    )
+}
+
+C001.propRequire= RequireProps
+
+export default C001
---
+++ src/components/faq/defaultValue.ts
@@ -0,0 +1,9 @@
+import { FaqProps } from "./types";
+
+export const RequireProps = {
+
+}
+
+export const data: FaqProps = {
+     
+}
